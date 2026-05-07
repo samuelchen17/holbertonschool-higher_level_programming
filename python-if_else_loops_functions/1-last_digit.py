969a972fa4be24083efb17fde6d9e4365386792b
Samuel Chen <sche842@gmail.com>
2026-05-07 19:21:47 +1000

    fix negative print

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,8 @@ import random
 
 number = random.randint(-10000, 10000)
 last_num = abs(number) % 10
+if number < 0:
+    last_num = -last_num
 
 print(f"Last digit of {number} is {last_num}", end=" ")
 
