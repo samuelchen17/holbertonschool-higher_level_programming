263ce2ad154485942da770f86b0e2eefc317bccc
Samuel Chen <sche842@gmail.com>
2026-05-08 12:41:00 +1000

    fix format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i, 10):
-        if i != j:
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        elif i != j:
             print("{}{}, ".format(i, j), end="")
-print("")
