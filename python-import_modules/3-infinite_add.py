d755d940bf8655f8fabc8e0dea368f86d815151b
Samuel Chen <sche842@gmail.com>
2026-05-08 15:54:05 +1000

    fix

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     result = 0
     for i in range(argc):
-        result += int(sys.argv[1:])
+        result += int(sys.argv[i + 1])
     print(result)
