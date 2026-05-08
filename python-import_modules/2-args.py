5e6db1dd9d20e7875cbe77a04152434e37eeb52b
Samuel Chen <sche842@gmail.com>
2026-05-08 15:36:50 +1000

    print args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
+
+    for i in range(argc):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
