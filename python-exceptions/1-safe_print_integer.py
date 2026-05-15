bfb255323a5dffc0c8ebd8b3822b60186010cb41
Samuel Chen <sche842@gmail.com>
2026-05-15 21:23:41 +1000

    include value error in exception

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
+    except (ValueError, TypeError):
         return False
