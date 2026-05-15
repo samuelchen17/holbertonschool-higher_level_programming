1e1d12371f59e5d907f124066aa38dfb6def3c1f
Samuel Chen <sche842@gmail.com>
2026-05-15 20:01:52 +1000

    tested

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,17 +5,16 @@ def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
 
-    values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
+    numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
 
     for i in range(len(roman_string)):
         if (
             i + 1 < len(roman_string)
-            and values[roman_string[i]] < values[roman_string[i + 1]]
+            and numerals[roman_string[i]] < numerals[roman_string[i + 1]]
         ):
-            total -= values[roman_string[i]]
+            total -= numerals[roman_string[i]]
         else:
-            total += values[roman_string[i]]
+            total += numerals[roman_string[i]]
 
     return total
