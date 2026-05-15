85a2d1b8e93c9a920a5520df30acb503ccd88d5e
Samuel Chen <sche842@gmail.com>
2026-05-15 19:09:15 +1000

    convert roman num

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string == None or not isinstance(roman_string, str):
+        return 0
+
+    numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    total = 0
+
+    for i in range(len(roman_string)):
+        if i + 1 == len(roman_string):
+            total += numerals[roman_string[i]]
+        elif numerals[roman_string[i]] < numerals[roman_string[i + 1]]:
+            total -= numerals[roman_string[i]]
+        else:
+            total += numerals[roman_string[i]]
+
+    return total
