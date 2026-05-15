3b9d1d1bea968b528ba721b2fe968b352c5d30cf
Samuel Chen <sche842@gmail.com>
2026-05-15 21:42:44 +1000

    remove index check

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,15 +6,11 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for i in range(x):
         try:
-            value = my_list[i]
-        except IndexError:
-            break
-
-        try:
-            print("{:d}".format(value), end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
             pass
 
     print()
+
     return count
