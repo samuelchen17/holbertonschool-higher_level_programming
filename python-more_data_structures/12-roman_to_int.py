e4fb72fba777f0c83cefb127317f0b63053281f4
Samuel Chen <sche842@gmail.com>
2026-05-15 19:10:42 +1000

    convert to uppercase

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
 
+    roman_string = roman_string.upper()
+
     numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
 
