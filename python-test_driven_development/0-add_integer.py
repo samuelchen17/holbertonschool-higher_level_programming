eee279a5ba1e39e71d165f1a8d438558229e5f9a
Samuel Chen <sche842@gmail.com>
2026-05-16 14:33:50 +1000

    tdd

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for adding two integers."""
+
+
+def add_integer(a, b=98):
+    """Adds two integers
+
+    return a + b
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
