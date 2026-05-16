a5fb272d19c2472ac2587917a35e78c27e020893
Samuel Chen <sche842@gmail.com>
2026-05-16 14:42:08 +1000

    add nan and inf test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -14,4 +14,7 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    a = int(a)
+    b = int(b)
+
+    return a + b
