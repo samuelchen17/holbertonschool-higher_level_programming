c1ec0237f3565cb1150711b904ba8c90bcc79f7b
Samuel Chen <sche842@gmail.com>
2026-05-17 13:21:45 +1000

    fix formatting

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,15 +9,23 @@ def matrix_divided(matrix, div):
 
     # must be list
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     # check list of lists
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     # elements must be int/float
-    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(
+        isinstance(element, (int, float)) for row in matrix for element in row
+    ):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     # all must be same size
     if not all(len(row) == len(matrix[0]) for row in matrix):
@@ -31,6 +39,8 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    new_matrix = [
+        [round(element / div, 2) for element in row] for row in matrix
+    ]
 
     return new_matrix
