6d7eff4d2e5bfca77af36ee04c3f429da57e053f
Samuel Chen <sche842@gmail.com>
2026-05-17 14:19:47 +1000

    add new tests and fix outputs

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,9 +4,15 @@
 
 def say_my_name(first_name, last_name=""):
     """Takes first and last name as arguments and prints the full name."""
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
+    if not isinstance(first_name, str) and not isinstance(last_name, str):
         raise TypeError(
             "first_name must be a string or last_name must be a string"
         )
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
 
     print(f"My name is {first_name} {last_name}")
