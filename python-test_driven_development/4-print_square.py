0f30a70a2f898b2bae6af12a4e599056cf334082
Samuel Chen <sche842@gmail.com>
2026-05-17 14:52:46 +1000

    print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Print a square."""
+
+
+def print_square(size):
+    """Print a square made of # with size as the length of the square"""
+    # non int, including float
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    # negatives
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
