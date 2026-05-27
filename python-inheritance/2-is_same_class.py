ec7a293d72ce43e3f3853cd75cf77a06987b0a34
Samuel Chen <sche842@gmail.com>
2026-05-27 21:20:56 +1000

    fix type comparison

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,4 +5,4 @@
 def is_same_class(obj, a_class):
     """ "function that returns True if the object is exactly an
     instance of the specified class otherwise False"""
-    return type(obj) == a_class
+    return type(obj) is a_class
