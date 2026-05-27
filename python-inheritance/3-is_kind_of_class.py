f7d7c3a8a9884408459a07a20b3fc1d7f66d7ad8
Samuel Chen <sche842@gmail.com>
2026-05-27 21:37:50 +1000

    isinstance check

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines a function that checks isinstance"""
+
+
+def is_same_class(obj, a_class):
+    """function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited
+    from, the specified class ; otherwise False."""
+    return isinstance(obj, a_class)
