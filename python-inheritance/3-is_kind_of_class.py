fa573835fd88acb2111aeaa434a20f2bb7f3c49d
Samuel Chen <sche842@gmail.com>
2026-05-27 21:38:42 +1000

    fix function name

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """Module that defines a function that checks isinstance"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """function that returns True if the object is an instance of,
     or if the object is an instance of a class that inherited
     from, the specified class ; otherwise False."""
