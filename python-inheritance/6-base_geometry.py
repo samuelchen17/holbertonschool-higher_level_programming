a49db3953520e9d9b3819d05a53a64257a188195
Samuel Chen <sche842@gmail.com>
2026-05-27 21:59:14 +1000

    raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines an Base Geometry class."""
+
+
+class BaseGeometry:
+    """Base geometry class."""
+
+    def area(self):
+        """Raises an exception."""
+        raise Exception("area() is not implemented")
