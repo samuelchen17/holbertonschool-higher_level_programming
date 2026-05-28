ba6778a8ee1aa1d3fa5a020bc9aec5a68af7a23c
Samuel Chen <sche842@gmail.com>
2026-05-28 20:56:40 +1000

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines square class."""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class."""
+
+    def __init__(self, size):
+        """Initialize square."""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """returns area of the rectangle"""
+        return self.__size**2
+
+    def __str__(self):
+        """returns square description."""
+        return f"[square] {self.__size}/{self.__size}"
