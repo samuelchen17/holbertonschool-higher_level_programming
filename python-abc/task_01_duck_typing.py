e7c56f4c4b9c498f953c718f337e1f989d3c2594
Samuel Chen <sche842@gmail.com>
2026-05-29 19:40:28 +1000

    value check

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -17,12 +17,20 @@ class Shape(ABC):
         """perimeter of shape"""
         pass
 
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
 
 class Circle(Shape):
     """Circle class"""
 
     def __init__(self, radius):
         """Initialize circle"""
+        super().integer_validator("radius", radius)
         self.__radius = radius
 
     def area(self):
@@ -39,6 +47,8 @@ class Rectangle(Shape):
 
     def __init__(self, width, height):
         """Initialize square."""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
 
