3051b743354021242ac794f58df076900c1a4c5e
Samuel Chen <sche842@gmail.com>
2026-06-02 21:36:54 +1000

    add exec

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+
+
 """This module contains a function that reads a text file"""
 
 
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
