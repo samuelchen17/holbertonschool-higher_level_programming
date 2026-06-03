c796d094d6636be1791d14292edf8478b4ea1674
Samuel Chen <sche842@gmail.com>
2026-06-03 21:12:17 +1000

    json dumps

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""This module contains a function"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON representation of an
+    object (string)"""
+    return json.dumps(my_obj)
