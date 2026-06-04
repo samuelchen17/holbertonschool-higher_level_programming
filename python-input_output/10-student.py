28a26cf9a497e6bd769f1d940479c4bae1203ad9
Samuel Chen <sche842@gmail.com>
2026-06-04 23:05:11 +1000

    attr arg

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""This module contains a class Student"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            result = {}
+
+            for key in attrs:
+                if hasattr(self, key):
+                    value = getattr(self, key)
+                    result[key] = value
+            return result
+
+        return self.__dict__
