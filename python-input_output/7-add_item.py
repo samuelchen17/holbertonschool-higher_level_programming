82a80fa85c1ccf89d557b90f9efe4c68ed130bae
Samuel Chen <sche842@gmail.com>
2026-06-04 21:37:34 +1000

    fix

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""This module contains a function"""
+
+import json
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    loaded = load_from_json_file("add_item.json")
+except:
+    loaded = []
+
+my_list = sys.argv[1:]
+new_list = loaded + my_list
+
+save_to_json_file(new_list, "add_item.json")
