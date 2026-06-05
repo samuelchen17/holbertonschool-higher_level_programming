21ae8283c75b556d7d25fe94c14ecd9e0a9f9056
Samuel Chen <sche842@gmail.com>
2026-06-05 21:27:41 +1000

    pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,30 @@
+"""this module contains functions related to serialization and deserialization"""
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(
+            f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}"
+        )
+
+    def serialize(self, filename):
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except:
+            return None
