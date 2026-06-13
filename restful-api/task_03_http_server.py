f9068be203f654339fbf4ec21406d864f38382a3
Samuel Chen <sche842@gmail.com>
2026-06-13 12:03:43 +1000

    create server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""This module contains functions for retrieving data from an API"""
+
+import http.server, json
+
+
+class ApiHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+
+def run(server_class=http.server.HTTPServer, handler_class=ApiHandler):
+    server_address = ("", 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
