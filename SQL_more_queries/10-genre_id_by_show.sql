86beaca5a74ba014bc22ca4aab2e372c9e869d32
Samuel Chen <sche842@gmail.com>
2026-06-19 19:56:56 +1000

    forgot to save changes

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,5 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows 
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id NOT NULL
-ORDER BY tv_show_genres.title 
-AND tv_show_genres.genre_id;
+ORDER BY tv_show_genres.title ASC,
+tv_show_genres.genre_id ASC;
