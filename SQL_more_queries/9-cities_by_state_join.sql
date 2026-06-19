854fdfd7a286a2e17837271808bbbe5fc0889828
Samuel Chen <sche842@gmail.com>
2026-06-19 19:02:14 +1000

    fix sort to order

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- list all cities in DB
 SELECT cities.id, cities.name, state.name 
-FROM cities JOIN state
+FROM cities 
+JOIN state
 ON cities.state_id = state.id
-SORT BY cities.id ASC;
+ORDER BY cities.id ASC;
