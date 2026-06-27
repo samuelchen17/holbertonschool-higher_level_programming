9852bec2a7062cd19f9d7f91528c184e830f46f2
Samuel Chen <sche842@gmail.com>
2026-06-27 16:57:11 +1000

    fix upper case only

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
 
     conn = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
@@ -22,7 +21,7 @@ if __name__ == "__main__":
         "SELECT id, name "
         "FROM states "
         "WHERE name "
-        "LIKE 'N%' "
+        "LIKE BINARY 'N%' "
         "ORDER BY id ASC"
     )
 
