812ba022caf8f972ba2cb0700525392479421528
Samuel Chen <sche842@gmail.com>
2026-06-27 17:11:58 +1000

    fix query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,17 +15,19 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        # default port = 3306
+        port=3306,
     )
 
     cur = conn.cursor()
 
     cur.execute(
-        "SELECT id, name "
-        "FROM states "
-        "WHERE name = '{}' "
-        "ORDER BY id ASC"
-    ).format(state_name)
+        (
+            "SELECT id, name "
+            "FROM states "
+            "WHERE BINARY name = '{}' "
+            "ORDER BY id ASC"
+        ).format(state_name)
+    )
 
     rows = cur.fetchall()
     for row in rows:
