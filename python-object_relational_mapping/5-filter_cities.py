dc4f13f6b2a8c04c97e84dedcf0c67116d6f9496
Samuel Chen <sche842@gmail.com>
2026-06-27 18:03:08 +1000

    fix query typo

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -21,9 +21,8 @@ if __name__ == "__main__":
 
     cur.execute("""
         SELECT cities.id, cities.name, states.name
-        FROM cities 
-        JOIN states
-        ON cities.state_id = states.id
+        FROM cities JOIN states
+        ON cities.state.id = states.id
         ORDER BY cities.id ASC
         """)
 
