7277fc2f927cbb81bedc39f5ce0aea1b91a7fa41
Samuel Chen <sche842@gmail.com>
2026-07-08 22:05:05 +1000

    3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+let x = process.argv[2];
+let y = process.argv[3];
+
+console.log(`${x} is ${y}`);
