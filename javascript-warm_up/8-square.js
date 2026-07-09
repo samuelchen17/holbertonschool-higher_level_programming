80a84a13667570a5806752b67e65dc199ecec72d
Samuel Chen <sche842@gmail.com>
2026-07-09 19:33:38 +1000

    square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+if (Number.isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    let row = '';
+    for (let j = 0; j < num; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
