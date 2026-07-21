4dc58dba3d2b52daabf6ab62226ad23ae114c69b
Samuel Chen <sche842@gmail.com>
2026-07-21 18:07:29 +1000

    fix key error

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -24,7 +24,9 @@ def items():
     with open("items.json", "r") as file:
         data = json.load(file)
 
-    return render_template("items.html", items=data["items"])
+    items = data.get("items", [])
+
+    return render_template("items.html", items=items)
 
 
 if __name__ == "__main__":
